572d81b26e4ad775d8f0670c3b1273d20f0d68f9
Suvidha-testing <suvidhakhegde@gmail.com>
2024-09-14 15:50:26 +0530

    Commiting

---
+++ src/sept_ex_04_11_2024/Lab002.java
@@ -0,0 +1,31 @@
+package sept_ex_04_11_2024;
+
+//Data types
+public class Lab002 {
+
+    public static void main(String[] args) {
+
+
+        //primitive data type
+
+        byte b=100; // 8 bits, 1 byte
+        short s=128; //16 bits, 2 byte
+        int i=1234;  //32 bits , 4 byte
+        long phone_number =12390908978l; //64 bits, 8 byte
+        long phone_number2 = 45656778897l; //64 bits, 8 byte
+        char grade ='D';
+        float f = 3.14f;  // 32 bits , 4 byte
+        double d = 112345455.556; //  64 bits , 8 byte
+
+        // non primitive data type
+
+        String name ="pramod";
+
+
+        long distance_earth_moon = 3480000l;
+        byte age= 100;
+
+
+    }
+
+}

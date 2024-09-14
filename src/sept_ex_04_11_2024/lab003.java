f0818805c1c57c1f029eeeefd6c45c78b8dcd05f
Suvidha-testing <suvidhakhegde@gmail.com>
2024-09-14 16:02:14 +0530

    Commiting 14-09

---
+++ src/sept_ex_04_11_2024/lab003.java
@@ -0,0 +1,16 @@
+package sept_ex_04_11_2024;
+
+public class lab003 {
+
+    public static void main(String[] args) {
+        char c1 ='c';
+        char c2='$';
+        char c3='\n';
+
+        System.out.println(c1);
+        System.out.println(c2);
+
+
+
+    }
+}

c39d7c13c0720fe894add077c07d704333b23c84
Suvidha-testing <suvidhakhegde@gmail.com>
2024-09-17 11:29:15 +0530

    Commiting 14-09

---
+++ src/ex_13_09_2024/Calculator.java
@@ -0,0 +1,21 @@
+package ex_13_09_2024;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        double a=34;
+        double b=10;
+        System.out.println("Simple Calculator !!");
+        System.out.printf(" Addition of %f and %f is %f ", a,b,(a+b));
+        System.out.println();
+        System.out.printf("Subtraction %f and %f is %f ", a,b , (a-b));
+        System.out.println();
+        System.out.printf(" Multiplication of  %f and %f is %f " , a,b,(a*b));
+        System.out.println();
+        System.out.printf("Division  %f and %f is %f" , a, b , (a/b));
+        System.out.println();
+        System.out.printf("Modulus  %f and %f is %f" , a , b ,(a%b));
+        System.out.println();
+
+    }
+}

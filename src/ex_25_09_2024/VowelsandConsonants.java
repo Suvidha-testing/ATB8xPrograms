56bbbce7b53ecc7b0f84e5c67c76f525139c6136
Suvidha-testing <suvidhakhegde@gmail.com>
2024-09-25 17:07:47 +0530

    Commiting 14-09

--- src/ex_25_09_2024/VowelsandConsonants.java
+++ src/ex_25_09_2024/VowelsandConsonants.java
@@ -3,7 +3,7 @@ package ex_25_09_2024;
 public class VowelsandConsonants {
     public static void main(String[] args) {
 
-        String name="Suvidha";
+        String name="PrAmod";
         int vowels=0;
         int consonants=0;
         for(int i=0;i<name.length();i++)

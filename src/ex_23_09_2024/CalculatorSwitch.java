f5abf84e9e0297ae8f9499f2902b6017663e4246
Suvidha-testing <suvidhakhegde@gmail.com>
2024-09-25 16:15:00 +0530

    Commiting 14-09

--- src/ex_23_09_2024/CalculatorSwitch.java
+++ src/ex_23_09_2024/CalculatorSwitch.java
@@ -1,9 +1,44 @@
 package ex_23_09_2024;
 
+import java.util.Scanner;
+
 public class CalculatorSwitch {
     public static void main(String[] args) {
 
 
+        int num1=20, num2=10;
+        char operation='*';
+        int result;
+        switch(operation) {
+            case '+':
+                result = num1+num2;
+                System.out.println("Result of addition is " +result);
+                break;
+
+            case '-':
+                result = num1-num2;
+                System.out.println("Result of Subtraction is " +result);
+                break;
+
+            case '*':
+                result = num1*num2;
+                System.out.println("Result of Multiplication is " +result);
+                break;
+
+            case '/':
+                result = num1/num2;
+                System.out.println("Result of division is " +result);
+                break;
+
+            case '%':
+                result = num1%num2;
+                System.out.println("Result of modulus is " +result);
+                break;
+
+            default:
+                System.out.println("Operation is not valid");
+                break;
+        }
 
     }
 }
--- src/ex_23_09_2024/ConvertUnitsSwitch.java
+++ src/ex_23_09_2024/ConvertUnitsSwitch.java
@@ -4,6 +4,26 @@ public class ConvertUnitsSwitch {
 
     public static void main(String[] args) {
 
+        double km=134.8;
+        double mile;
+        double fahrenheit;
+        double celsius=100.9;
+        int user_choice=2;
+        switch(user_choice)
+        {
+
+            case 1:
+                mile=km*0.6214;
+                System.out.println(km +" converted in miles is "+ mile);
+                break;
+            case 2:
+                fahrenheit=(celsius)*9/5+32;
+                System.out.println(celsius +" celsius converted to fahrenheit value is " +fahrenheit +"f");
+                break;
+            default:
+                System.out.println("invalid input value");
+                break;
+        }
 
     }
 }
